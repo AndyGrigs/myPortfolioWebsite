353c03a191210b8221cfd9ea16f1bdbe2930395f
Andriy <andygrigs88@gmail.com>
2024-08-18 13:22:37 +0200

    editing App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,7 @@
 const App= () => {
   return (
     <section>
-      {/* <Navbar /> */}
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/certification" element={<Certification />} />
-        <Route path="/contacts" element={<Contacts />} />
-      </Routes> */}
+   
     </section>
   );
 };
